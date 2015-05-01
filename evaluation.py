5d7c2052164e2e56f1ed1c190d9ea610dcb23657
Wendy Woodin <woodin@college.harvard.edu>
2015-05-01 08:22:51 -0400

    reorganizing

--- evaluation.py
+++
@@ -1,8 +0,0 @@
-def evaluate(predictions, answers):
-    index = range(len(predictions))
-    correct = 0
-    for i in index:
-        if predictions[i] == answers[i]:
-           correct += 1
-    total = len(predictions)
-    return (float(correct)/float(total)*100.)
